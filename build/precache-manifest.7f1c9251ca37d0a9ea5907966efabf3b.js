2d0874d22921e0433a8792eb7403ca959618638e
Kevin Gervais Gilman <kevingervaisgilman@MacBook-Pro-de-Kevin.local>
2019-03-18 21:44:16 -0400

    add build

---
+++ build/precache-manifest.7f1c9251ca37d0a9ea5907966efabf3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68ec9bc09ad327ea2710",
+    "url": "/static/js/main.8d1db61b.chunk.js"
+  },
+  {
+    "revision": "460f501a1090cfa7d59d",
+    "url": "/static/js/2.fb486f88.chunk.js"
+  },
+  {
+    "revision": "68ec9bc09ad327ea2710",
+    "url": "/static/css/main.a7b24fc3.chunk.css"
+  },
+  {
+    "revision": "f26fd158924a2fbfd2d1a421507bd085",
+    "url": "/index.html"
+  }
+];
